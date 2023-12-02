5495a6b1a9193d089622a8db2730b057a97e5918
tranphu69 <tranphuth0609@gmail.com>
2023-12-02 09:21:02 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponent extends React.Component{
+    render(){
+        return(
+            <div> my first component </div>
+        );
+    }
+}
+
+export default MyComponent;
