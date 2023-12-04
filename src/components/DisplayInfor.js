157329a35b19b84e94787f448175d33149986164
tranphu69 <tranphuth0609@gmail.com>
2023-12-04 15:38:03 +0700

    video 33

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -3,16 +3,35 @@ import './DisplayInfor.scss';
 import logo from "./../logo.svg";
 
 class DisplayInfor extends React.Component{
-    state = {
-        isShowListUser: true
+    constructor(props){
+        super(props);
+        this.state = {
+            isShowListUser: true
+        }
+        console.log(">>> call constructor: 1");
     }
     handleShowhide = () => {
         this.setState({
             isShowListUser: !this.state.isShowListUser
         })
     }
+    componentDidMount(){
+        console.log(">>> call me component did mount");
+        setTimeout(() => {
+            document.title = "Tran Phu la toi"
+        }, 3000);
+    }
+    componentDidUpdate(prevProps, prevState, snapshot){
+        console.log(">>> call me component did update", this.props, prevProps)
+        if(this.props.listUsers !== prevProps.listUsers){
+            if(this.props.listUsers.length === 5){
+                alert("you got 5 user")
+            }
+        }
+    }
 
     render(){
+        console.log(">>> call me render: ")
         const { listUsers } = this.props;
         return(
             <div>
@@ -22,7 +41,6 @@ class DisplayInfor extends React.Component{
                 { this.state.isShowListUser &&
                     <div>
                         {listUsers.map((user) => {
-                            console.log(user);
                             return(
                                 <div key={user.id} className={user.age > 18 ? "green" : "red"}>
                                     <div>
