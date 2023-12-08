968a61baa464567325fec22142902708232ea6cb
tranphu69 <tranphuth0609@gmail.com>
2023-12-08 11:36:05 +0700

    video 53

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,11 +18,13 @@ const Header = () => {
             <NavLink to="/admin" className='nav-link'> Admin </NavLink>
           </Nav>
           <Nav>
-            <NavDropdown title="Settings" id="basic-nav-dropdown">
+            <button className='btn-login'>Log in</button>
+            <button className='btn-signup'>Sign up</button>
+            {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item>Log in</NavDropdown.Item>
               <NavDropdown.Item>Log out</NavDropdown.Item>
               <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,21 @@
+import videoHomePage from '../../assets/video-homepage.mp4'
+
 const HomePage = (props) => {
     return(
         <div className="homepage-container">
-            home page component
+            <video autoPlay muted loop>
+                <source src={videoHomePage} type = "video/mp4"/>
+            </video>
+            <div className='homepage-content'>
+                <div className='title-1'>There's a better way to ask</div>
+                <div className='title-2'>You don't want to make a boring form.
+                    And your audience won't answer one.
+                    Create a typeform instead-and make everyone happy.
+                </div>
+                <div >
+                    <button className='title-3'>Get's started. It's free</button>
+                </div>
+            </div>
         </div>
     )
 }
