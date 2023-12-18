b5ce9b1448930282eadf095d1e45840ff94b2f39
tranphu69 <tranphuth0609@gmail.com>
2023-12-18 18:52:50 +0700

    video 100

--- src/components/Admin/Content/Quiz/ManageQuiz.js
+++ src/components/Admin/Content/Quiz/ManageQuiz.js
@@ -7,6 +7,8 @@ import TableQuiz from './TableQuiz';
 import Accordion from 'react-bootstrap/Accordion';
 import ModalDeleteQuiz from './ModalDeleteQuiz';
 import ModalEditQuiz from './ModalEditQuiz';
+import QuizQA from './QuizQA';
+import AssignQuiz from './AssignQuiz';
 
 const options = [
     { value: 'EASY', label: 'EASY' },
@@ -108,17 +110,29 @@ const ManageQuiz = (props) => {
                             </div>
                         </fieldset>
                     </div>
+                    <div className="list-detail">
+                        <TableQuiz
+                            listQuiz = {listQuiz}
+                            handleClickBtnDelete={handleClickBtnDelete}
+                            fetchListQuizzes = {fetchListQuizzes}
+                            handleClickBtnEdit = {handleClickBtnEdit}
+                        />
+                    </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Update Q/A Quizzes</Accordion.Header>
+                    <Accordion.Body>
+                        <QuizQA/>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>Assign to Users</Accordion.Header>
+                    <Accordion.Body>
+                        <AssignQuiz/>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
-            <div className="list-detail">
-                <TableQuiz
-                    listQuiz = {listQuiz}
-                    handleClickBtnDelete={handleClickBtnDelete}
-                    fetchListQuizzes = {fetchListQuizzes}
-                    handleClickBtnEdit = {handleClickBtnEdit}
-                />
-            </div>
             <ModalDeleteQuiz
                 show={showModalDeleteQuiz}
                 setShow={setShowModalDeleteQuiz}
